ce9e9a2b6c652f307f575ff56eb294d3896def41
Nidhyaa <143414953+Nidhyaa@users.noreply.github.com>
2024-06-03 23:14:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,378 @@
+// Inserting the Images
+// Inserting the Images
+function insertImage() {
+
+    document.querySelectorAll('.box').forEach(image => {
+
+        if (image.innerText.length !== 0) {
+            if (image.innerText == 'WRicochet' || image.innerText == 'BRicochet') {
+                image.innerHTML = `${image.innerText} <img class='allimg allpawn' src="${image.innerText}.png" alt="">`
+                image.style.cursor = 'pointer'
+
+            }
+
+            else {
+
+                image.innerHTML = `${image.innerText} <img class='allimg' src="${image.innerText}.png" alt="">`
+                image.style.cursor = 'pointer'
+            }
+        }
+    })
+}
+insertImage()
+
+
+//Coloring
+
+function coloring() {
+    const color = document.querySelectorAll('.box')
+
+    color.forEach(color => {
+
+        getId = color.id
+        arr = Array.from(getId)
+        arr.shift()
+        aside = eval(arr.pop())
+        aup = eval(arr.shift())
+        a = aside + aup
+
+        if (a % 2 == 0) {
+            color.style.backgroundColor = 'rgb(240, 201, 150)'
+        }
+        if (a % 2 !== 0) {
+            color.style.backgroundColor = 'rgb(100, 75, 43)'
+        }
+        // if (a % 2 == 0) {
+        //     color.style.backgroundColor = 'seagreen'
+        // }
+        // if (a % 2 !== 0) {
+        //     color.style.backgroundColor = 'lime'
+        // }
+
+    })
+}
+coloring()
+
+
+
+
+//function to not remove the same team element
+
+function reddish() {
+    document.querySelectorAll('.box').forEach(i1 => {
+        if (i1.style.backgroundColor == 'pink') {
+
+            document.querySelectorAll('.box').forEach(i2 => {
+
+                if (i2.style.backgroundColor == 'green' && i2.innerText.length !== 0) {
+
+
+                    greenText = i2.innerText
+
+                    pinkText = i1.innerText
+
+                    pinkColor = ((Array.from(pinkText)).shift()).toString()
+                    greenColor = ((Array.from(greenText)).shift()).toString()
+
+                    getId = i2.id
+                    arr = Array.from(getId)
+                    arr.shift()
+                    aside = eval(arr.pop())
+                    aup = eval(arr.shift())
+                    a = aside + aup
+
+                    if (a % 2 == 0 && pinkColor == greenColor) {
+                        i2.style.backgroundColor = 'rgb(240, 201, 150)'
+                    }
+                    if (a % 2 !== 0 && pinkColor == greenColor) {
+                        i2.style.backgroundColor = 'rgb(100, 75, 43)'
+                    }
+
+                    // if (pinkColor == greenColor) {
+                    //     i2.style.backgroundColor = 'rgb(253, 60, 60)'
+                    // }
+                }
+            })
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
+
+tog = 1
+whiteCastleChance=true
+blackCastleChance=true
+
+document.querySelectorAll('.box').forEach(item => {
+
+
+
+    item.addEventListener('click', function () {
+
+        // To delete the opposite element
+
+        if (item.style.backgroundColor == 'green' && item.innerText.length == 0) {
+            tog = tog + 1
+        }
+        else if (item.style.backgroundColor == 'aqua' && item.innerText.length == 0) {
+            tog = tog + 1
+        }
+
+        else if (item.style.backgroundColor == 'green' && item.innerText.length !== 0) {
+
+            document.querySelectorAll('.box').forEach(i => {
+                if (i.style.backgroundColor == 'pink') {
+                    pinkId = i.id
+                    pinkText = i.innerText
+
+                    document.getElementById(pinkId).innerText = ''
+                    item.innerText = pinkText
+                    coloring()
+                    insertImage()
+                    tog = tog + 1
+
+                }
+            })
+        }
+
+
+
+        getId = item.id
+        arr = Array.from(getId)
+        arr.shift()
+        aside = eval(arr.pop())
+        arr.push('0')
+        aup = eval(arr.join(''))
+        a = aside + aup
+
+
+
+        // Function to display the available paths for all pieces
+
+        function whosTurn(toggle) {
+
+            // KING
+
+            if (item.innerText == `${toggle}titan`) {
+
+
+                if (aside < 8) {
+                    document.getElementById(`b${a + 1}`).style.backgroundColor = 'green'
+
+                }
+                if (aside > 1) {
+
+                    document.getElementById(`b${a - 1}`).style.backgroundColor = 'green'
+                }
+                if (aup < 800) {
+
+                    document.getElementById(`b${a + 100}`).style.backgroundColor = 'green'
+                }
+                if (aup > 100) {
+
+                    document.getElementById(`b${a - 100}`).style.backgroundColor = 'green'
+                }
+
+                if (aup > 100 && aside < 8) {
+
+                    document.getElementById(`b${a - 100 + 1}`).style.backgroundColor = 'green'
+                }
+                if (aup > 100 && aside > 1) {
+
+                    document.getElementById(`b${a - 100 - 1}`).style.backgroundColor = 'green'
+                }
+                if (aup < 800 && aside < 8) {
+
+                    document.getElementById(`b${a + 100 + 1}`).style.backgroundColor = 'green'
+                }
+                if (aup < 800 && aside > 1) {
+
+                    document.getElementById(`b${a + 100 - 1}`).style.backgroundColor = 'green'
+                }
+
+                item.style.backgroundColor = 'pink'
+
+            }
+
+
+            // TANK
+            if (item.innerText == `${toggle}tank`) {
+
+
+                if (aside < 8) {
+                    document.getElementById(`b${a + 1}`).style.backgroundColor = 'green'
+
+                }
+                if (aside > 1) {
+
+                    document.getElementById(`b${a - 1}`).style.backgroundColor = 'green'
+                }
+                if (aup < 800) {
+
+                    document.getElementById(`b${a + 100}`).style.backgroundColor = 'green'
+                }
+                if (aup > 100) {
+
+                    document.getElementById(`b${a - 100}`).style.backgroundColor = 'green'
+                }
+
+                if (aup > 100 && aside < 8) {
+
+                    document.getElementById(`b${a - 100 + 1}`).style.backgroundColor = 'green'
+                }
+                if (aup > 100 && aside > 1) {
+
+                    document.getElementById(`b${a - 100 - 1}`).style.backgroundColor = 'green'
+                }
+                if (aup < 800 && aside < 8) {
+
+                    document.getElementById(`b${a + 100 + 1}`).style.backgroundColor = 'green'
+                }
+                if (aup < 800 && aside > 1) {
+
+                    document.getElementById(`b${a + 100 - 1}`).style.backgroundColor = 'green'
+                }
+
+                item.style.backgroundColor = 'pink'
+
+            }
+            
+
+        }
+
+
+        // Toggling the turn
+
+        if (tog % 2 !== 0) {
+            document.getElementById('tog').innerText = "White's Turn"
+            whosTurn('W')
+        }
+        if (tog % 2 == 0) {
+            document.getElementById('tog').innerText = "Black's Turn"
+            whosTurn('B')
+        }
+
+        reddish()
+
+
+
+        // winning()
+
+        numOfKings = 0
+
+
+        document.querySelectorAll('.box').forEach(win => {
+            if (win.innerText == 'Wking' || win.innerText == 'Bking') {
+                numOfKings += 1
+            }
+
+        })
+
+        if (numOfKings == 1) {
+            setTimeout(() => {
+                // console.log(`${toggle}`) 
+                if (tog % 2 == 0) {
+                    alert('White Wins !!')
+                    location.reload()
+                }
+                else if (tog % 2 !== 0) {
+                    alert('Black Wins !!')
+                    location.reload()
+                }
+            }, 100)
+        }
+
+
+
+    })
+
+})
+
+
+
+
+
+// Moving the element
+document.querySelectorAll('.box').forEach(item => {
+
+    item.addEventListener('click', function () {
+
+
+        if (item.style.backgroundColor == 'pink') {
+
+            pinkId = item.id
+            pinkText = item.innerText
+
+            document.querySelectorAll('.box').forEach(item2 => {
+
+                item2.addEventListener('click', function () {
+
+                    getId = item2.id
+                    arr = Array.from(getId)
+                    arr.shift()
+                    aside = eval(arr.pop())
+                    arr.push('0')
+                    aup = eval(arr.join(''))
+                    a = aside + aup
+
+                    if (item2.style.backgroundColor == 'green' && item2.innerText.length == 0) {
+
+                        if (pinkText == `Wpawn` && aup == 800) {
+
+                            document.getElementById(`b${a}`).innerText = 'Wtitan'
+                            document.getElementById(pinkId).innerText = ''
+                            coloring()
+                            insertImage()
+
+                        }
+                        else if (pinkText == `Bpawn` && aup == 100) {
+
+                            document.getElementById(`b${a}`).innerText = 'Btitan'
+                            document.getElementById(pinkId).innerText = ''
+                            coloring()
+                            insertImage()
+
+                        }
+                        else {
+
+
+
+                            document.getElementById(pinkId).innerText = ''
+                            item2.innerText = pinkText
+                            coloring()
+                            insertImage()
+                        }
+
+                    }
+
+                })
+            })
+
+        }
+
+    })
+
+})
+
+
+
+
+
+
+// Prvents from selecting multiple elements
+z = 0
+document.querySelectorAll('.box').forEach(ee => {
+    ee.addEventListener('click', function () {
+        z = z + 1
+        if (z % 2 == 0 && ee.style.backgroundColor !== 'green' && ee.style.backgroundColor !== 'aqua') {
+            coloring()
+        }
+    })
+})
